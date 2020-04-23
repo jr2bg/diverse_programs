6b12915c6c18e83e2c9e503afedf2580bca2bacb
jr2bg <j.arturo.b.g@gmail.com>
2020-04-22 19:53:06 -0500

    RK iteration2function + LV beginning

--- bio_math/lotka_volterra.py
+++ bio_math/lotka_volterra.py
@@ -1,53 +1,43 @@
 import matplotlib.pyplot as plt
-import numpy as np
+
 # paso
-h = 0.01
+h = 0.1
 
 # número de pasos
 N = 1000
-
-# t
-#t = np.array([i * h for i in range(N)])
-t = [i * h for i in range(N)]
 
 # número máximo de argumentos para las funciones, y funciones
 m = 2
 
 # inicialización del diccionario de argumentos
 keys_arg = ["w_{}".format(i) for i in range(1, m + 1)]
-d_arg = {}
 
+# parámetros de Lotka-Volterra
+alpha = 1.1
+beta = 0.4
+gamma = 0.1
+delta = 0.4
 
-
-# definir todas las funciones!!!
-# def f_i(t_j = t_j, **kwargs):
-
-# def f_1(t_j, **kwargs):
-#     return -4 * kwargs["w_1"] + 3 * kwargs["w_2"] + 6
-#
-# def f_2(t_j , **kwargs):
-#     return -2.4 * kwargs["w_1"] + 1.6 * kwargs["w_2"] +3.6
-
-mu = 1
-
+# funciones de Lotka-Volterra
 def f_1(t_j, **kwargs):
-    return -kwargs["w_2"] + kwargs["w_1"]* (mu - kwargs["w_1"]**2 - kwargs["w_2"]**2)
-def f_2(t_j, **kwargs):
-    return kwargs["w_1"] + kwargs["w_2"]* (mu - kwargs["w_1"]**2 - kwargs["w_2"]**2)
+    return alpha * kwargs["w_1"] - beta * kwargs["w_1"] * kwargs["w_2"]
 
+def f_2(t_j, **kwargs):
+    return delta * kwargs["w_1"] * kwargs["w_2"] - delta * kwargs["w_2"]
 
 # tupla de funciones
 t_functions = (f_1, f_2) #f_3,...., f_m
 
-#w_0 = [0,0]
-w_0 = [7.,8.]
-#w = np.zeros((N, 2))
-#w[0] = w_0
-w = [w_0]
+# condiciones iniciales
+w_0 = [10.,10.]
+
 
 def iteration_RK(t_functions, N, h, w_0, keys_arg):
+    # array de tiempo
     t = [i * h for i in range(N)]
+    # diccionario con los argumentos
     d_arg = {}
+    # array con los valores de w_j
     w = [w_0]
 
     for j in range(N-1):
@@ -93,24 +83,13 @@ def iteration_RK(t_functions, N, h, w_0, keys_arg):
         for i in range(m):
             w_i.append(w[j][i] + (k_1[i] + 2 * k_2[i] + 2 * k_3[i] + k_4[i]) / 6)
 
-        #print(j)
-        # print(k_1)
-        # print(k_2)
-        # print(k_3)
-        # print(k_4)
-        #print(w_i)
         w.append(w_i)
 
-    return w
+    return t, w
 
 
 
-
-
-w = iteration_RK(t_functions, N, h, w_0, keys_arg)
-plt.plot([z[0] for z in w],[z[1] for z in w])
-# plt.plot(t, [z[0] for z in w])
-# plt.plot(t, [z[1] for z in w])
-# plt.xlabel("tiempo")
-# plt.ylabel("W")
+t,w = iteration_RK(t_functions, N, h, w_0, keys_arg)
+plt.plot(t,[z[0] for z in w])
+plt.plot(t,[z[1] for z in w])
 plt.show()
